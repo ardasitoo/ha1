24712f7938c3a16f72408e2761782b32816a70de
Simar Keskinoglu <simar.ks@icloud.com>
2025-10-14 13:17:25 +0200

    division

--- src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -88,6 +88,22 @@ class CalculatorTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    @DisplayName("should display result after dividing two positive numbers")
+    void testPositiveDivision() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(6);
+        calc.pressBinaryOperationKey("/");
+        calc.pressDigitKey(2);
+        calc.pressEqualsKey();
+
+        String expected = "3";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
+
 
     //TODO hier weitere Tests erstellen
 }
